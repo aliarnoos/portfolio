abeb437ee93006a6438d89a19cffed0c2aad7257
aliarnoos <alimarnoos@gmail.com>
2022-12-25 18:58:42 +0300

    remove scroll effect

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 
-// $(document).ready(function(){
-//     $(window).scroll(function(){
-//         let scroll = $(window).scrollTop();
-//         if (scroll > 300) {
-//           $(".nav").css("background" , "blue");
-//         }
+// // $(document).ready(function(){
+// //     $(window).scroll(function(){
+// //         let scroll = $(window).scrollTop();
+// //         if (scroll > 300) {
+// //           $(".nav").css("background" , "blue");
+// //         }
   
-//         else{
-//             $(".nav").css("background" , "#333");  	
-//         }
-//     })
-//   })
+// //         else{
+// //             $(".nav").css("background" , "#333");  	
+// //         }
+// //     })
+// //   })
 
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 80) {
-    document.getElementById("nav").classList.add("scroll");
-  } else {
-    document.getElementById("nav").classList.remove("scroll");
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 80) {
+//     document.getElementById("nav").classList.add("scroll");
+//   } else {
+//     document.getElementById("nav").classList.remove("scroll");
+//   }
+// }
